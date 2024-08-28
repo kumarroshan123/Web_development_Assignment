8a6c633a96f1dca82088890286c1e86c026f7626
kumarroshan123 <kumar30roshan@gmail.com>
2024-08-28 16:52:21 +0530

    Added_Profile_Model

--- Django/blogapp/blog/models.py
+++ Django/blogapp/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user_roles=(('reader','Reader'),('author','Author'))
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type= models.CharField(max_length=10,choices=user_roles)
+
+    def __str__(self) -> str:
+        return f'{self.user}-{self.user_type}'
+    
+# class Post(mo)
