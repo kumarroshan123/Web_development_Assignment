81a8603c769ed5d8bbee7ac2ef409542867a09c6
kumarroshan123 <kumar30roshan@gmail.com>
2024-08-28 17:08:56 +0530

    Added_ProfileSerializer

--- Django/blogapp/blog/serializers.py
+++ Django/blogapp/blog/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Profile,Post
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=["id","username","password"]
+
+
+class ProfileSerializer(serializer.ModelSerializer):
+    class Meta:
+        model=Profile 
+        fields='__all__'
+
 
