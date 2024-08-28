af4716af443dde6ac792dab090e0e5ffcdaedbf9
kumarroshan123 <kumar30roshan@gmail.com>
2024-08-28 16:57:36 +0530

    added_Post_Model

--- Django/blogapp/blog/models.py
+++ Django/blogapp/blog/models.py
@@ -11,4 +11,14 @@ class Profile(models.Model):
     def __str__(self) -> str:
         return f'{self.user}-{self.user_type}'
     
-# class Post(mo)
+class Post(models.Model):
+    title= models.CharField(max_length=100)
+    content=models.TextField()
+    published=models.BooleanField(default=False)
+    author=models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created_at=models.DateTimeField( auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'{self.title}-{self.author}'
+    
