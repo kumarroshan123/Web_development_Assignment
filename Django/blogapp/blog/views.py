30e678bbc0aec384657d7ae48d24eac5c84df6f9
kumarroshan123 <kumar30roshan@gmail.com>
2024-08-28 17:35:16 +0530

    Added_RegisterViewClass_In_Views

--- Django/blogapp/blog/views.py
+++ Django/blogapp/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView,Response
+from rest_framework import status
+from .serializers import UserSerializer,PostSerializer,ProfileSerializer
+from .models import Profile
 # Create your views here.
+
+class  RegisterView(APIView):
+
+    def post(self,request):
+        serializer=UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user=serializer.save()
+            profile=Profile(user=user,user_type= request.data.get('user_type'))
+            profile.save()
+            return Response({'message':"Signup Successful"}, status=status.HTTP_201_CREATED)
+        return Response({'message':"Invalid Credentials"}, status=status.HTTP_406_NOT_ACCEPTABLE)
