b0c6c943570d044e71847cb16ed09f5c74b94f27
kumarroshan123 <kumar30roshan@gmail.com>
2024-09-11 23:47:20 +0530

    CoercionInArthmetic

---
+++ Welcome to JavaScript/JavascriptFundamental3/CoericionInArthmetic.js
@@ -0,0 +1,10 @@
+function coercionInArthmetic(a,b){
+    console.log(a+b);
+    console.log(a-b);
+    console.log(a*b);
+    console.log(a/b);
+}
+
+let a= 5;
+let b= "10";
+coercionInArthmetic(a,b);
