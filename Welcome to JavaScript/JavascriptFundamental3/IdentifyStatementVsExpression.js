d7f882ea99a56d7c4c2c7c43922ca68ee6f15453
kumarroshan123 <kumar30roshan@gmail.com>
2024-09-12 00:18:16 +0530

    IdentifyStatementVsExpression

---
+++ Welcome to JavaScript/JavascriptFundamental3/IdentifyStatementVsExpression.js
@@ -0,0 +1,7 @@
+// Statements: let x = 5;, if (x > 3) {...}, and console.log(...).
+// Expressions: 2 + 3, and (x > 3) ? 'greater' : 'lesser'.
+let y = 2 + 3;
+
+for(let i=0;i<5;i++){
+    console.log(i,y);
+}
