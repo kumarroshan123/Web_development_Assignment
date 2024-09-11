7bc9ac3675ebcc99e83472cc25af3409fc385820
kumarroshan123 <kumar30roshan@gmail.com>
2024-09-12 01:52:15 +0530

    BasicDoWhileLoop

---
+++ Welcome to JavaScript/JavascriptFundamental3/basicDoWhileLoop.js
@@ -0,0 +1,8 @@
+let i=10;
+do{
+   console.log(i);
+   i++;
+}while(i<=5)
+
+    // while: No output because i starts at 10 and the condition i <= 5 is false.
+    // do...while: Outputs 10 once because the loop body executes before the condition is checked.
