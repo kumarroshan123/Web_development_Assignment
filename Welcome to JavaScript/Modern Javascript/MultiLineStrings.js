4071a2758ccca1eee3d6d56cf2d310a54750bc71
kumarroshan123 <kumar30roshan@gmail.com>
2024-09-18 21:52:19 +0530

    MultiLineString

---
+++ Welcome to JavaScript/Modern Javascript/MultiLineStrings.js
@@ -0,0 +1,5 @@
+function generateParagraph(){
+    return `The gentle breeze swept across the golden fields, \n carrying with it the scent of wildflowers and freshly cut grass. Birds chirped melodiously in the distance, while the sun cast a warm, \n golden glow over the landscape. The sky, painted in hues of orange and pink, signaled the approaching evening. In the midst of this serene setting,\n a small stream gurgled quietly, \n its clear waters reflecting the beauty of the surroundings. It was a moment of peace, a brief pause from the hustle and bustle of daily life,\n where nature offered a quiet sanctuary to those willing to listen.`
+}
+
+console.log(generateParagraph());
