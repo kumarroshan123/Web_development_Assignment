eeaef0a0c2fde2ff134ae48a4b6992b65ae5cb14
kumarroshan123 <kumar30roshan@gmail.com>
2024-11-06 15:01:19 +0530

    Problem7_Home_page

---
+++ Welcome to JavaScript/LocalStorageAndIntroToDOM/Problem7.js/script.js
@@ -0,0 +1,51 @@
+let text = document.querySelector("#text");
+let option = document.querySelector("#options");
+let tablebody = document.querySelector(".tablebody");
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+
+function Addtodo() {
+  let textval = text.value;
+  let optionvalue = option.value;
+  if (textval) {
+    let obj = { title: textval, priority: optionvalue, status: "Pending" };
+    todo.push(obj);
+    localStorage.setItem("todo", JSON.stringify(todo));
+    display();
+    text.value="";
+  } else {
+    alert("Todo cannot be Empty");
+  }
+}
+
+function display() {
+    tablebody.innerHTML = "";
+    todo.forEach((ele,i) => {
+    let body = document.createElement("tr");
+    body.innerHTML = `<td>${ele.title}</td>
+              <td>${ele.priority}</td>
+              <td><select id="status-choice${i}">
+            <option value="Completed" ${ele.status==="Completed"? "selected":""}>Completed</option>
+            <option value="Pending" ${ele.status==="Pending"? "selected":""}>Pending</option>
+          </select></td>
+              <td><button onclick="Archieve(${i})">Archieve</button></td>`;
+
+
+    let statuschoice=body.querySelector(`#status-choice${i}`);
+    statuschoice.addEventListener('change',()=>{
+        todo[i].status=statuschoice.value;
+        localStorage.setItem("todo", JSON.stringify(todo));
+    });
+    tablebody.append(body);
+  });
+}
+
+function Archieve(i){
+    let ele=todo[i];
+    let archive= JSON.parse(localStorage.getItem("archive")) || [];
+    archive.push(ele);
+    localStorage.setItem("archive",JSON.stringify(archive));
+    todo=todo.filter((a)=>a!==ele);
+    localStorage.setItem("todo",JSON.stringify(todo));
+    display();
+}
+display();
