b70fab7ca1a8562a5210b919e478caa80c4c8c49
kumarroshan123 <kumar30roshan@gmail.com>
2024-11-19 20:40:49 +0530

    Completed

--- React/React Core State Management/Problem7/vite-project/src/components/Homepage.jsx
+++ React/React Core State Management/Problem7/vite-project/src/components/Homepage.jsx
@@ -4,6 +4,8 @@ import Product from "./Product";
 
 const HomePage = () =>{
     let [data,setData]=useState([]);
+    let [cursor,setCursor]= useState({x:0,y:0});
+
     useEffect(()=>{
         let d=async()=>{
             let res= await fetch("https://jsonplaceholder.typicode.com/users");
@@ -12,8 +14,16 @@ const HomePage = () =>{
         }
         d();
     },[]);
+    useEffect(()=>{
+       function movement(e){
+           setCursor({x:e.screenX,y:e.screenY});
+       }
+       document.addEventListener("mousemove",movement);
+    });
+    
     return (
     <div>
+        <h2>Cursor Co-ordinates: X={cursor.x} Y={cursor.y}</h2>
         {data.map((ele)=><Product key={ele.id}  {...ele} />)}
     </div>
     );
