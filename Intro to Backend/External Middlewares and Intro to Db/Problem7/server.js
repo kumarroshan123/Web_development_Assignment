8fb6bd0affd7e5556222b5cd82fd72f441a182b9
kumarroshan123 <kumar30roshan@gmail.com>
2025-01-18 15:02:24 +0530

    Problem7

---
+++ Intro to Backend/External Middlewares and Intro to Db/Problem7/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
+const app = express();
+const upload = multer({ dest: "src/uploads" });
+
+cloudinary.config({
+    cloud_name: 'dezldcx0r',
+    api_key: '342259184431556',
+    api_secret: '7qujoSVAVDU0aCBzCU2Nu282Wic',
+  });
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/src/index.html");
+  });
+
+app.post("/upload",upload.single("file"),async(req,res)=>{
+    try{
+        const result=await cloudinary.uploader.upload(req.file.path);
+        res.status(200).json({ message: "File uploaded successfully", imageUrl: result.secure_url })
+    }catch(err){
+        res.status(500).json({ message: "Upload failed", error: err.message });
+    }
+})
+
+app.listen(3000, () =>{
+     console.log("Server running on http://localhost:3000")
+});
